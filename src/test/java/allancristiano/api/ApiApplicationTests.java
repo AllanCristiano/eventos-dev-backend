3555cc736d278648ec3daf7f1ad7549866eb2320
Allan Cristiano da Silva Santos <drmundo920@gmail.com>
2024-06-19 09:13:31 -0300

    feat: initial commit

---
+++ src/test/java/allancristiano/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package allancristiano.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
