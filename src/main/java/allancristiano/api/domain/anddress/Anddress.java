fb8f5d09449a69fac89b532d70c1d4fd09786a86
Allan Cristiano da Silva Santos <drmundo920@gmail.com>
2024-06-19 09:25:26 -0300

    feat: creating entities

---
+++ src/main/java/allancristiano/api/domain/anddress/Anddress.java
@@ -0,0 +1,37 @@
+package allancristiano.api.domain.anddress;
+
+import java.util.UUID;
+
+import allancristiano.api.domain.event.Event;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Table(name = "andress")
+@Entity
+
+@Getter
+@Setter
+
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Anddress {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String city;
+    private String uf;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+}
